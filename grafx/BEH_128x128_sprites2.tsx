cb804970c28e3f8b45f804ac6a90da800ce74a9a
langel <knoxburry@gmail.com>
2023-07-24 02:07:13 -0400

    added early assets

---
+++ grafx/BEH_128x128_sprites2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="BEH 128x128 sprites2" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="beh-tiles.bmp" width="128" height="128"/>
+</tileset>
