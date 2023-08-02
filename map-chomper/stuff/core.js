8444fac7006411444a805e83582cac8c43e21ee2
langel <knoxburry@gmail.com>
2023-08-02 15:57:57 -0400

    updating tiles chr

--- map-chomper/stuff/core.js
+++ map-chomper/stuff/core.js
@@ -4,21 +4,45 @@ let tohex = (x) => x.toString(16).padStart(2, '0');
 const br = "<br>";
 let output;
 
-const process = (file, data) => {
+const process_json = (data) => {
+	// XXX deprecated as we aren't using tiled any longer
 	data = JSON.parse(data);
 	console.log(data);
-	const map_w = data.width;
-	const map_h = data.height;
-	data = data.layers[0].data;
-	let table = 'map_data_0_table:\n';
+	spew(data.layers[0].data.map(x => x - 1), data.width, data.height);
+}
+
+const process_map = (data) => {
+	console.log(data);
+	spew(data, data[data.length - 4], data[data.length - 2]);
+}
+
+const spew = (data, map_w, map_h) => {
+	let table = 'map_0_tiles:\n';
+	console.log(map_w + ' ' + map_h);
+	// map tiles
 	for (let x = 0; x < map_w; x++) {
 		let line = '';
-		for (let y = 0; y < map_h; y++) {
+		for (let y = 2; y < map_h; y++) {
 			let val = data[x + y * map_w];
-			val--;
 			if (val > 255) val = 0;
 			line += tohex(val);
 		}
+		table += ' hex ' + line + '\n';
+	}
+	// map attributes
+	table += '\nmap_0_attributes:\n';
+	const attr_offset = map_w * map_h;
+	const attr_w = map_w >> 2;
+	const attr_h = map_h >> 2;
+	console.log(attr_w + ' ' + attr_h);
+	for (let x = 0; x < attr_w; x++) {
+		let line = '';
+		for (let y = 0; y < attr_h; y++) {
+			console.log(attr_offset + x + y * attr_h);
+			let val = data[attr_offset + x + y * attr_h];
+			line += tohex(val);
+		}
+		console.log(line);
 		table += ' hex ' + line + '\n';
 	}
 	output.innerHTML = '<pre>' + table + '</pre>';
@@ -38,12 +62,16 @@ window.addEventListener("DOMContentLoaded", () => {
 			if (item.kind === 'file') {
 				const file = item.getAsFile();
 				const r = new FileReader();
-				//r.readAsArrayBuffer(file);
 				//r.readAsBinaryString(file);
-				r.readAsText(file);
-				r.onload = () => {
-					process(file, r.result);
-				};
+				console.log(file);
+				if (file.name.includes('.json')) {
+					r.readAsText(file);
+					r.onload = () => process_json(r.result);
+				}
+				if (file.name.includes('.map')) {
+					r.readAsArrayBuffer(file);
+					r.onload = () => process_map(new Uint8Array(r.result));
+				}
 			}
 		});
 	});
