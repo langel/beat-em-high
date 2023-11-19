609eceae4d8273baf6c88abcb87ac888a239646b
langel <knoxburry@gmail.com>
2023-11-18 23:28:52 -0500

    clear sprites routine again

--- common.asm
+++ common.asm
@@ -53,6 +53,20 @@ clear_ram: subroutine
         bne .loop	; loop 256 times
         rts
         
+clear_sprites: subroutine
+	lda #$ff
+        ldx #$00
+.loop
+	sta $0200,x
+        inx
+        bne .loop
+        rts
+        ; other
+        hex 0f081a38
+        hex 0f0a391a
+        
+        hex 0f041536 ; pink krok
+        
 
 ; wait for VSYNC to start
 wait_sync:
